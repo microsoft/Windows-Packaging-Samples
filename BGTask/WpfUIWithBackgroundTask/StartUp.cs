fc00dac7e2f7e9680d316fb8c32118f8e8c02953
ridomin <ridomin@live.com>
2017-11-03 18:05:02 -0700

    Start point. Basic WPF App.

---
+++ BGTask/WpfUIWithBackgroundTask/StartUp.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Windows;
+
+namespace WpfUIWithBackgroundTask
+{
+    public class StartUp
+    {
+        [System.STAThread]
+        public static void Main(string[] args)
+        {
+            var wrapper = new Rido.SingleInstance.SingleInstanceManager<App>();
+            wrapper.Run(args);
+        }
+    }
+
+    public partial class App : System.Windows.Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+    }
+}
