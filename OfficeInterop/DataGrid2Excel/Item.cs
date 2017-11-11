f72ca8ca8f50c36ccf5deaa33c5a8dd73baef63c
ridomin <ridomin@live.com>
2017-11-10 18:23:38 -0800

    Add Office interop sample

---
+++ OfficeInterop/DataGrid2Excel/Item.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid2Excel
+{
+    public class Item : INotifyPropertyChanged
+    {
+        private string id;
+        private int quantity;
+        private string description;
+        private double unitPrice;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                if (quantity != value)
+                {
+                    quantity = value;
+                    OnPropertyChanged("Quantity");
+                }
+            }
+        }
+
+        public double UnitPrice
+        {
+            get { return unitPrice; }
+            set
+            {
+                if (unitPrice != value)
+                {
+                    unitPrice = value;
+                    OnPropertyChanged("UnitPrice");
+                }
+            }
+        }
+    }
+}
